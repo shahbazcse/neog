15c0b375d5a8f69e03d08f7c64b3d36499df2ab6
shahbaz-cse <cseshahbaz@gmail.com>
2022-09-03 20:10:57 +0530

    Completed markEleven

---
+++ markEleven/script.js
@@ -0,0 +1,33 @@
+var DoB = document.querySelector(".input-date");
+var luckyNo = document.querySelector(".input-number");
+var btnCheck = document.querySelector(".btn-check");
+var output = document.querySelector(".output");
+
+btnCheck.addEventListener("click",() => processValues());
+
+var processValues = () => {
+    if( (DoB.value) && (luckyNo.value) ){
+        var sumDOB = calculateDateSum();
+        checkLuckyNumber(sumDOB);
+    } else {
+        output.innerHTML = "Please enter both the fields";
+    }
+}
+var calculateDateSum = () => {
+    var date = DoB.value.replaceAll("-","");
+    var sum = 0;
+    for(let digit of date){
+        sum = sum + Number(digit);
+    }
+    return sum;
+}
+
+var checkLuckyNumber = (sumDOB) => {
+    if((sumDOB)%luckyNo.value === 0){
+        output.innerHTML = `🥳 ${luckyNo.value} is a lucky number for you! 🥳`;
+    } else {
+        output.innerHTML = `${luckyNo.value} is not that lucky number for you 😕`;
+    }
+}
+
+
