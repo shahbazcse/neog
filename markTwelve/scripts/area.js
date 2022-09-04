5a1c5fcc58b1f81050c2ac1113560bccc66e5ae4
shahbaz-cse <cseshahbaz@gmail.com>
2022-09-05 00:55:39 +0530

    Completed markTwelve

---
+++ markTwelve/scripts/area.js
@@ -0,0 +1,28 @@
+
+var input = document.querySelectorAll("input");
+var output = document.querySelector(".output");
+var btnCalculate = document.querySelector("button");
+
+var processValues = () => {
+    var a = Number(input[0].value);
+    var b = Number(input[1].value);
+    var c = Number(input[2].value);
+    if(a && b && c){
+        var result = findArea(a,b,c);
+        output.innerHTML = `Area of Triangle = ${result}`;
+    } else {
+        output.innerHTML = "Please enter all the fields";
+    }
+}
+
+var findArea = (a,b,c) => {
+    var height = Math.sqrt(4*(a*a*b*b)-(Math.pow(((a*a)+(b*b)-(c*c)),2)));
+    var area = 0.25*height;
+    /*
+    var height = Math.sqrt(4*(b*b)-(c*c));
+    var area = 0.25*c*height;
+    */
+    return area;
+}
+
+btnCalculate.addEventListener("click", () => processValues());
---
+++ markTwelve/scripts/hypotenuse.js
@@ -0,0 +1,26 @@
+var input = document.querySelectorAll("input");
+var output = document.querySelector(".output");
+var btnCalculate = document.querySelector("button");
+
+var processValues = () => {
+    var a = Number(input[0].value);
+    var b = Number(input[1].value);
+    if(a && b){
+        var result = findHypotenuse(a,b);
+        output.innerHTML = `Hypotenuse = ${result}`;
+    }
+    else{
+        output.innerHTML = "Please enter both fields";
+    }
+}
+
+var findHypotenuse = (a,b) => {
+    var hypotenuse = Math.sqrt((a*a)+(b*b));
+    return hypotenuse;
+}
+
+btnCalculate.addEventListener("click", () => processValues());
+
+/*
+function "processValues" must be called on line 24 after the function definition on line 9
+*/
